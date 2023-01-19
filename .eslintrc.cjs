c94eb8b6595987b1fd1606478c442d8c73005e49
dom <domwilson95@hotmail.co.uk>
2023-01-19 10:44:53 +0000

    Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    "extends": [
+    extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
     ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "tsconfigRootDir": __dirname,
-        "project": ['./tsconfig.eslint.json'],
+    overrides: [],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.eslint.json"],
     },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+    plugins: ["@typescript-eslint"],
+    rules: {},
+};
