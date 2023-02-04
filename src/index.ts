207fc372f4e15545561a2536acfbc0e4eb793357
dom <domwilson95@hotmail.co.uk>
2023-02-04 11:28:52 +0000

    Updated Exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 export { Node, NodeAim, NodeType } from "./tree/node.js";
 export { Tree } from "./tree/tree.js";
 export { SearchOpts, SearchExit, PruningType, SearchMethod } from "./tree/search.js";
-export { EvaluateNodeFunc, GetMovesFunc, CreateChildNodeFunc } from "./tree/interfaces.js";
+export { EvaluateNodeFunc, GetMovesFunc, CreateChildNodeFunc, GetScoresFunc } from "./tree/interfaces.js";
 export { Negamax } from "./negamax/negamax.js";
 export { NegamaxResult, NegamaxOpts } from "./negamax/index.js";
 export { SortMethod } from "./tree/sorting.js";
 export { Minimax } from "./minimax/minimax.js";
 export { MinimaxOpts, MinimaxResult } from "./minimax/index.js";
+export { Maxn } from "./maxn/maxn.js";
+export { MaxnOpts, MaxnResult } from "./maxn/index.js";
