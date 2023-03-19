7301fffba92a6d85f389b1ed087a81e59775debd
domw95 <63186722+domw95@users.noreply.github.com>
2023-03-19 22:28:26 +0000

    Improved docs for search options and their interactions (#46)
    * Updated pruneByPathLength docs
    
    * Corrected timeout option docs
    
    * Fixed link to optimal option
    
    * Improved `NegamaxOpts.optimal`  docs

--- src/tree/search.ts
+++ src/tree/search.ts
@@ -47,7 +47,7 @@ export const enum PruningType {
 export class SearchOpts {
     /** Method used to search for best move. See {@link SearchMethod} for details*/
     method = SearchMethod.DEPTH;
-    /** Timeout used for **all** searches. Timeout disabled when set to 0.*/
+    /** Timeout used when method is {@link SearchMethod.TIME}. Timeout disabled when set to 0.*/
     timeout = 0;
     /** Default search depth for depth and deepening searches.*/
     depth = 0;
@@ -74,14 +74,18 @@ export class SearchOpts {
     /** Method used to sort nodes if {@link SearchOpts.presort} is enabled */
     sortMethod = SortMethod.DEFAULT;
     /** Set to `true` to shorten winning paths and lengthen losing paths.
-     *  Only works when combined with {@link PruningType.ALPHA_BETA | PruningType.ALPHA_BETA}
+     *  Only works when combined with {@link PruningType.ALPHA_BETA | PruningType.ALPHA_BETA}.
+     * Is disabled for {@link Negamax} when {@link NegamaxOpts.optimal} is `true`.
+     *
+     * Check {@link Negamax}, {@link Minimax} or {@link Maxn} (and corresponing Opts) for
+     * specific support.
      */
     pruneByPathLength = false;
     /**
      * @experimental
      * Select randomly from all of the children with the best value.
      *
-     * Check {@link Negamax}, {@link Minimax} or {@link Maxn} for
+     * Check {@link Negamax}, {@link Minimax} or {@link Maxn} (and corresponing Opts) for
      * specific support.
      */
     randomBest = false;
@@ -93,7 +97,7 @@ export class SearchOpts {
      * - randomWeight = 1, equal probabilty
      * - randomWeight = 5, 5 times more likely for every +1 valuation
      *
-     * Check {@link Negamax}, {@link Minimax} or {@link Maxn} for
+     * Check {@link Negamax}, {@link Minimax} or {@link Maxn} (and corresponing Opts) for
      * specific support.
      */
     randomWeight = 0;
