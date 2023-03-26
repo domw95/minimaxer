dd9dd7e4d42bd055d5826d12ff9b8f200739bc35
dom <domwilson95@hotmail.co.uk>
2023-03-26 20:49:42 +0100

    Node removal tests

--- tests/node_removal.ts
+++ tests/node_removal.ts
@@ -1,22 +1,38 @@
 import * as mancala from "../examples/games/mancala.js";
 import * as mx from "../dist/index.js";
+import { validateDescendants } from "../dist/tree/utils.js";
 
-test("Node removal", () => {
-    const game = new mancala.mancala();
-    game.start();
-    let root = new mx.Node(mx.NodeType.ROOT, game.clone(), 0, 0, mx.NodeAim.MAX, game.moves);
-    let tree = new mx.Negamax(root);
-    tree.CreateChildNode = mancala.createChildCallback;
+test("Remove every depth", () => {
+    for (let depth = 1; depth <= 8; depth++) {
+        const game = new mancala.mancala();
+        game.start();
 
-    tree.opts.depth = 8;
-    let now = performance.now();
-    tree.opts.method = mx.SearchMethod.DEEPENING;
-    const result = tree.evaluate();
-    console.log("Search took: %d", performance.now() - now);
-    console.log(result);
+        // Standard search
+        let root = new mx.Node(mx.NodeType.ROOT, game.clone(), 0, 0, mx.NodeAim.MAX, game.moves);
+        let tree = new mx.Negamax(root);
+        tree.CreateChildNode = mancala.createChildCallback;
+        tree.opts.optimal = true;
+        tree.opts.method = mx.SearchMethod.DEEPENING;
+        tree.opts.depth = depth;
 
-    now = performance.now();
-    tree.removeNodes();
-    console.log("Removal took: %d", performance.now() - now);
-    console.log("%d Nodes remaining", tree.nodeCount);
+        let result = tree.evaluate();
+        validateDescendants(tree.root);
+        expect(tree.nodeCount).toBe(tree.root.descendantCount + 1);
+
+        // Removal search
+        root = new mx.Node(mx.NodeType.ROOT, game.clone(), 0, 0, mx.NodeAim.MAX, game.moves);
+        tree = new mx.Negamax(root);
+        tree.CreateChildNode = mancala.createChildCallback;
+        tree.opts.optimal = true;
+        tree.opts.method = mx.SearchMethod.DEEPENING;
+        tree.opts.depth = depth;
+        tree.opts.removalMethod = mx.RemovalMethod.ALWAYS;
+
+        let result2 = tree.evaluate();
+
+        expect(result2.move).toBe(result.move);
+        expect(result2.value).toBe(result.value);
+        validateDescendants(tree.root);
+        expect(tree.nodeCount).toBe(tree.root.descendantCount + 1);
+    }
 });
