dd8dc1a20698704d888cb8bf68342b8bffd4c2be
dom <domwilson95@hotmail.co.uk>
2023-03-26 22:48:24 +0100

    Updated negamax example for node remove

--- examples/negamax_mancala.ts
+++ examples/negamax_mancala.ts
@@ -6,7 +6,7 @@ const game = new mancala.mancala();
 game.start();
 
 // Perform a search to depth 4
-console.log("Search 1\n");
+console.log("Search 1: Depth 4\n");
 let root = new mx.Node(mx.NodeType.ROOT, game.clone(), 0, 0, mx.NodeAim.MAX, game.moves);
 let tree = new mx.Negamax(root);
 tree.CreateChildNode = mancala.createChildCallback;
@@ -15,7 +15,7 @@ tree.opts.method = mx.SearchMethod.DEPTH;
 console.log(tree.evaluate());
 
 // Perform a search deepening with alpha-beta pruning to depth 4, printing the result at each depth
-console.log("\nSearch 2\n");
+console.log("\nSearch 2: Deepening with depth callback\n");
 root = new mx.Node(mx.NodeType.ROOT, game.clone(), 0, 0, mx.NodeAim.MAX, game.moves);
 tree = new mx.Negamax(root);
 tree.CreateChildNode = mancala.createChildCallback;
@@ -27,7 +27,7 @@ tree.depthCallback = (tree, result) => {
 console.log(tree.evaluate());
 
 // Perform a time limited optimal search
-console.log("\nSearch 3\n");
+console.log("\nSearch 3: Optimal Time\n");
 root = new mx.Node(mx.NodeType.ROOT, game.clone(), 0, 0, mx.NodeAim.MAX, game.moves);
 tree = new mx.Negamax(root);
 tree.CreateChildNode = mancala.createChildCallback;
@@ -37,11 +37,23 @@ tree.opts.method = mx.SearchMethod.TIME;
 console.log(tree.evaluate());
 
 // Select randomly from the best moves
-console.log("\nSearch 4\n");
+console.log("\nSearch 4: Random best selectiom\n");
 root = new mx.Node(mx.NodeType.ROOT, game.clone(), 0, 0, mx.NodeAim.MAX, game.moves);
 tree = new mx.Negamax(root);
 tree.CreateChildNode = mancala.createChildCallback;
 tree.opts.depth = 2;
 tree.opts.randomBest = true;
 tree.opts.method = mx.SearchMethod.DEPTH;
+console.log(tree.evaluate());
+
+// Remove nodes after each depth when node count >= 1000
+console.log("\nSearch 5: Node removal\n");
+root = new mx.Node(mx.NodeType.ROOT, game.clone(), 0, 0, mx.NodeAim.MAX, game.moves);
+tree = new mx.Negamax(root);
+tree.CreateChildNode = mancala.createChildCallback;
+tree.opts.depth = 8;
+tree.opts.optimal = true;
+tree.opts.method = mx.SearchMethod.DEEPENING;
+tree.opts.removalMethod = mx.RemovalMethod.COUNT;
+tree.opts.removalCount = 1000;
 console.log(tree.evaluate());
